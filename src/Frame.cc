5245023c7de785e3147ebd23dc1d6ce4a5aead19
AndroidStudio2017 <17319255967@163.com>
2020-06-13 00:26:40 +0800

    加入双目立体匹配部分的注释

--- src/Frame.cc
+++ src/Frame.cc
@@ -57,15 +57,30 @@ Frame::Frame(const Frame &frame)
         SetPose(frame.mTcw);
 }
 
-
+// 双目SLAM中的Frame帧构造函数
+// 
+// Input:
+//      imLeft:             左目图像
+//      imRight:            右目图像
+//      timeStamp:          时间戳
+//      extractorLeft:      左目图像的ORB特征提取器
+//      extractorRight:     右目图像的ORB特征提取器
+//      voc:                Vocabulary
+//      distCoef:           畸变参数
+//      bf:                 基线长度
+//      thDepth:            深度阈值
+// Output:
+//      构造函数，无返回值
 Frame::Frame(const cv::Mat &imLeft, const cv::Mat &imRight, const double &timeStamp, ORBextractor* extractorLeft, ORBextractor* extractorRight, ORBVocabulary* voc, cv::Mat &K, cv::Mat &distCoef, const float &bf, const float &thDepth)
     :mpORBvocabulary(voc),mpORBextractorLeft(extractorLeft),mpORBextractorRight(extractorRight), mTimeStamp(timeStamp), mK(K.clone()),mDistCoef(distCoef.clone()), mbf(bf), mThDepth(thDepth),
      mpReferenceKF(static_cast<KeyFrame*>(NULL))
 {
     // Frame ID
+    // 同单目一样，总帧数加一，nNextId是一个静态成员变量
     mnId=nNextId++;
 
     // Scale Level Info
+    // 同单目一样，设定一些图像金字塔的参数
     mnScaleLevels = mpORBextractorLeft->GetLevels();
     mfScaleFactor = mpORBextractorLeft->GetScaleFactor();
     mfLogScaleFactor = log(mfScaleFactor);
@@ -75,18 +90,29 @@ Frame::Frame(const cv::Mat &imLeft, const cv::Mat &imRight, const double &timeSt
     mvInvLevelSigma2 = mpORBextractorLeft->GetInverseScaleSigmaSquares();
 
     // ORB extraction
-    thread threadLeft(&Frame::ExtractORB,this,0,imLeft);
+    // 这里为了加速，开启两个线程分别计算左目和右目的ORB特征
+    thread threadLeft(&Frame::ExtractORB,           // 线程函数，就是之前单目用到的ExtractORB
+                    this,                           // 该类的函数
+                    0,                              // 0 是ExtractORB的参数，表示提取左目图像特征
+                    imLeft);                        // 也是ExtractORB参数，表示左目图像
     thread threadRight(&Frame::ExtractORB,this,1,imRight);
+
+    // 这里的join()方法，需要主线程等待threadLeft执行完才可以向下进行
     threadLeft.join();
+    // 同理
     threadRight.join();
 
+    // mvKeys保存了左图提取的特征点，N是左图特征点的数量
     N = mvKeys.size();
 
+    // 如果特征点数量为0，则返回
     if(mvKeys.empty())
         return;
 
+    // 对左图提取的特征进行去畸变操作
     UndistortKeyPoints();
 
+    // 计算左右图的双目立体匹配，与单目帧的主要区别
     ComputeStereoMatches();
 
     mvpMapPoints = vector<MapPoint*>(N,static_cast<MapPoint*>(NULL));    
@@ -170,7 +196,7 @@ Frame::Frame(const cv::Mat &imGray, const cv::Mat &imDepth, const double &timeSt
     AssignFeaturesToGrid();
 }
 
-// 帧的构造函数
+// 单目帧的构造函数
 // 
 // Input:
 //      imGray:         待构造帧对象的灰度图像
@@ -552,83 +578,128 @@ void Frame::ComputeImageBounds(const cv::Mat &imLeft)
     }
 }
 
+// 进行双目帧构造时的双目立体匹配
+// 
+// Input:
+//      均是利用成员变量进行操作
+// Output:
+//      输出也是到成员变量中
+// 原理见笔记（八）中1
 void Frame::ComputeStereoMatches()
 {
+    // mvuRight[iL]保存的是，左图中第i个特征点所对应的右图特征点的索引
+    // mvDepth[iL]保存的是，左图中第i个特征点所对应的深度
     mvuRight = vector<float>(N,-1.0f);
     mvDepth = vector<float>(N,-1.0f);
 
+    // ORB特征描述子的距离阈值，用于进行粗匹配
+    // TH_HIGH是100，TH_LOW是50，这里取了个平均
     const int thOrbDist = (ORBmatcher::TH_HIGH+ORBmatcher::TH_LOW)/2;
 
+    // 取左图原图的行数，也就是高
     const int nRows = mpORBextractorLeft->mvImagePyramid[0].rows;
 
     //Assign keypoints to row table
+    // vRowIndices即是在统计行特征点，vRowIndices[i]保存了右图中第i行所有特征点的列坐标，也就是x坐标
+    // 比如 vRowIndices[0] = [1, 4, 5], 则说明右图第0行第1列有一个特征点，第0行第4列有一个特征点，第0行第5列有一个特征点
     vector<vector<size_t> > vRowIndices(nRows,vector<size_t>());
 
+    // 预分配空间
     for(int i=0; i<nRows; i++)
         vRowIndices[i].reserve(200);
 
+    // 右图提取特征点的总数，左图的特征点总数的成员变量N
     const int Nr = mvKeysRight.size();
 
+    // 遍历右图所有特征点，统计行特征点
     for(int iR=0; iR<Nr; iR++)
     {
+        // 取得右图中的特征点
         const cv::KeyPoint &kp = mvKeysRight[iR];
-        const float &kpY = kp.pt.y;
-        const float r = 2.0f*mvScaleFactors[mvKeysRight[iR].octave];
+        const float &kpY = kp.pt.y;         // 特征点的y坐标，也就是行坐标
+
+        // 因为左右目图像经过了立体匹配，那么可能在y方向会有不准确的地方
+        // 那么我们设定一个r，如果kpY处有特征点p，那么[kpY - r, kpY + r]，这些行，都有该特征点
+        // r随着特征点金字塔层数的不同而不同，金字塔层数越高，不确定度越高，r越大
+        const float r = 2.0f*mvScaleFactors[mvKeysRight[iR].octave];    
         const int maxr = ceil(kpY+r);
         const int minr = floor(kpY-r);
 
+        // 这里就是将[minr, maxr]，这些行都加入索引为iR的特征点
         for(int yi=minr;yi<=maxr;yi++)
             vRowIndices[yi].push_back(iR);
     }
 
     // Set limits for search
+    // 这里不知道为啥将深度的最小值设置为基线长度？？？
     const float minZ = mb;
+    // 视差的最小值肯定是0
     const float minD = 0;
+    // 根据上面设置的最小深度，由视差公式得到最大视差，视差公式见笔记（八）2
     const float maxD = mbf/minZ;
 
     // For each left keypoint search a match in the right image
+    // vDistIdx[iL].first是索引为iL的左图特征点匹配右图特征点对应的描述子距离
+    // vDistIdx[IL].second是索引为iL的左图特征点匹配的右图特征点的索引
     vector<pair<int, int> > vDistIdx;
-    vDistIdx.reserve(N);
+    vDistIdx.reserve(N);        // 预分配内存
 
+    // 遍历左图特征点
     for(int iL=0; iL<N; iL++)
     {
+        // 取得左图特征点
         const cv::KeyPoint &kpL = mvKeys[iL];
-        const int &levelL = kpL.octave;
-        const float &vL = kpL.pt.y;
-        const float &uL = kpL.pt.x;
+        const int &levelL = kpL.octave;     // 所处金字塔层数
+        const float &vL = kpL.pt.y;         // 左图特征点的y坐标，行坐标
+        const float &uL = kpL.pt.x;         // 左图特征点的x坐标，列坐标
 
+        // 刚才统计的行坐标点，就是右图中和左图特征点同行(vL)的所有特征点
         const vector<size_t> &vCandidates = vRowIndices[vL];
 
+        // 如果没有同行的，那么就没有匹配点，下一个
         if(vCandidates.empty())
             continue;
 
+        // 上面计算的最大视差，也就是确定枚举对比的理论范围
+        // 右图的视差肯定在左边，所以最大的U就设定为uL-minD
         const float minU = uL-maxD;
         const float maxU = uL-minD;
 
+        // 如果最大范围小于0，则没有
         if(maxU<0)
             continue;
 
+        // 用于求取最小值，一开始设为TH_HIGH的意思就是，如果距离比这个还大，那肯定不可能
         int bestDist = ORBmatcher::TH_HIGH;
-        size_t bestIdxR = 0;
+        size_t bestIdxR = 0;    // 保存粗匹配的索引
 
+        // 获得左图特征点对应的描述子
         const cv::Mat &dL = mDescriptors.row(iL);
 
         // Compare descriptor to right keypoints
+        // 遍历右图中候选的特征点
         for(size_t iC=0; iC<vCandidates.size(); iC++)
         {
+            // 获取特征点索引以及特征点
             const size_t iR = vCandidates[iC];
             const cv::KeyPoint &kpR = mvKeysRight[iR];
 
+            // 如果左图特征点所在的金字塔层数和右图特征点所在的金字塔层数相差超过2，则被认为误差太大，舍弃
             if(kpR.octave<levelL-1 || kpR.octave>levelL+1)
                 continue;
 
+            // 右图特征点的x坐标，列坐标
             const float &uR = kpR.pt.x;
 
+            // 如果特征点的x坐标，列坐标，超过理论的范围，那么舍弃
+            // 如果符合理论的范围，则通过计算左右图特征点描述子之间的距离，选取最小的距离结果作为粗匹配的结果
             if(uR>=minU && uR<=maxU)
             {
+                // 取出右图特征点对应的描述子
                 const cv::Mat &dR = mDescriptorsRight.row(iR);
-                const int dist = ORBmatcher::DescriptorDistance(dL,dR);
+                const int dist = ORBmatcher::DescriptorDistance(dL,dR);     // 计算左右图描述子之间的距离
 
+                // 计算最大值，并保存最大值的索引，作为粗匹配结果
                 if(dist<bestDist)
                 {
                     bestDist = dist;
@@ -638,51 +709,73 @@ void Frame::ComputeStereoMatches()
         }
 
         // Subpixel match by correlation
+        // 精确匹配
+        // 如果上面粗匹配得到的最好的匹配距离大于等于阈值，则舍弃
+        // 否则进行接下来的处理
         if(bestDist<thOrbDist)
         {
             // coordinates in image pyramid at keypoint scale
-            const float uR0 = mvKeysRight[bestIdxR].pt.x;
-            const float scaleFactor = mvInvScaleFactors[kpL.octave];
-            const float scaleduL = round(kpL.pt.x*scaleFactor);
-            const float scaledvL = round(kpL.pt.y*scaleFactor);
-            const float scaleduR0 = round(uR0*scaleFactor);
+            const float uR0 = mvKeysRight[bestIdxR].pt.x;       // 上述粗匹配得到的特征点的x坐标，列坐标
+            const float scaleFactor = mvInvScaleFactors[kpL.octave];    // 左图特征点对应的金字塔层比例因子的倒数
+            const float scaleduL = round(kpL.pt.x*scaleFactor);     // 左图特征点x坐标乘比例因子，换算为该层金字塔图像的坐标
+            const float scaledvL = round(kpL.pt.y*scaleFactor);     // 左图特征点y坐标乘比例因子，换算为该层金字塔图像的坐标
+            const float scaleduR0 = round(uR0*scaleFactor);     // 将粗匹配得到的右图特征点的x坐标也换算为金字塔图像中的坐标
 
             // sliding window search
+            // w是窗口边长的一半，实际窗口大小是2w × 2w
             const int w = 5;
+            // 获取左图中图像金字塔该层图像，以左图特征点为中心，2w为边长的窗口
             cv::Mat IL = mpORBextractorLeft->mvImagePyramid[kpL.octave].rowRange(scaledvL-w,scaledvL+w+1).colRange(scaleduL-w,scaleduL+w+1);
-            IL.convertTo(IL,CV_32F);
+            IL.convertTo(IL,CV_32F);        // 转换格式
+
+            // 进行归一化，去除亮度的影响，其实就是将窗口内所有像素减去一个左图特征点的像素值
             IL = IL - IL.at<float>(w,w) *cv::Mat::ones(IL.rows,IL.cols,CV_32F);
 
+            // 同样是为了求最小距离设置的变量
             int bestDist = INT_MAX;
-            int bestincR = 0;
+            int bestincR = 0;       // 最小距离对应的特征点索引
+
+            // 窗口要滑动的长度是L，即开始的窗口是[scaleduR0-L-w, scaleduR0-L+w]，最终的窗口是[scaleduR0+L-w, scaleduR0+L+w]
             const int L = 5;
             vector<float> vDists;
             vDists.resize(2*L+1);
 
+            // 这里iniu是不是写错了？？？ 应该是scaleduR0-L-w？？？
+            // 其实就是为了限制下面取子矩阵的时候不越界
             const float iniu = scaleduR0+L-w;
             const float endu = scaleduR0+L+w+1;
+            // 如果会越界，就跳过
             if(iniu<0 || endu >= mpORBextractorRight->mvImagePyramid[kpL.octave].cols)
                 continue;
 
+            // 滑动窗口，找到最优的匹配点
             for(int incR=-L; incR<=+L; incR++)
             {
+                // 获取滑动窗口，右图中的窗口
                 cv::Mat IR = mpORBextractorRight->mvImagePyramid[kpL.octave].rowRange(scaledvL-w,scaledvL+w+1).colRange(scaleduR0+incR-w,scaleduR0+incR+w+1);
                 IR.convertTo(IR,CV_32F);
+                // 同样去除亮度的影响，和上述左图是一样的操作
                 IR = IR - IR.at<float>(w,w) *cv::Mat::ones(IR.rows,IR.cols,CV_32F);
 
+                // 这里是比较两个图像块的相似度，用sad计算
                 float dist = cv::norm(IL,IR,cv::NORM_L1);
+                // 同样是求取距离最小，并记录滑动的步数
                 if(dist<bestDist)
                 {
                     bestDist =  dist;
                     bestincR = incR;
                 }
 
-                vDists[L+incR] = dist;
+                vDists[L+incR] = dist;  // 记录每个滑动步数对应的像素相似度（sad距离）
             }
 
+            // 如果最小值出现在最左和最右，则舍弃
+            // 这里也不知道为什么要舍弃这两种情况？？？？
             if(bestincR==-L || bestincR==L)
                 continue;
 
+            // 亚像素插值
+            // 这部分见笔记（八）3后的公式
             // Sub-pixel match (Parabola fitting)
             const float dist1 = vDists[L+bestincR-1];
             const float dist2 = vDists[L+bestincR];
@@ -690,37 +783,45 @@ void Frame::ComputeStereoMatches()
 
             const float deltaR = (dist1-dist3)/(2.0f*(dist1+dist3-2.0f*dist2));
 
+            // 应该也是要求，直观上来理解，亚像素插值这个结果应该小于1，亚像素就是不足一个像素
             if(deltaR<-1 || deltaR>1)
                 continue;
 
             // Re-scaled coordinate
+            // 将粗匹配的右图特征点位置加上精确计算后的窗口滑动步数，再加上亚像素匹配的结果
+            // 然后恢复到原图的x坐标，列坐标
             float bestuR = mvScaleFactors[kpL.octave]*((float)scaleduR0+(float)bestincR+deltaR);
 
+            // 求视差
             float disparity = (uL-bestuR);
 
+            // 如果视差又满足一开始我们设定的理论视差范围，那么则记录视差
             if(disparity>=minD && disparity<maxD)
             {
+                // 如果视差小于0，那么限定视差为0.01
                 if(disparity<=0)
                 {
                     disparity=0.01;
                     bestuR = uL-0.01;
                 }
+                // 设置索引为iL的特征点深度
                 mvDepth[iL]=mbf/disparity;
-                mvuRight[iL] = bestuR;
-                vDistIdx.push_back(pair<int,int>(bestDist,iL));
+                mvuRight[iL] = bestuR;      // 设置匹配的特征点列坐标
+                vDistIdx.push_back(pair<int,int>(bestDist,iL));     // 设置iL特征点匹配的最小sad距离，用于去除离群点
             }
         }
     }
 
+    // 按照距离进行排序
     sort(vDistIdx.begin(),vDistIdx.end());
-    const float median = vDistIdx[vDistIdx.size()/2].first;
-    const float thDist = 1.5f*1.4f*median;
+    const float median = vDistIdx[vDistIdx.size()/2].first;     // 找到中位数
+    const float thDist = 1.5f*1.4f*median;                      // 设置距离阈值
 
     for(int i=vDistIdx.size()-1;i>=0;i--)
     {
-        if(vDistIdx[i].first<thDist)
+        if(vDistIdx[i].first<thDist)        // 如果距离小于阈值，则无操作
             break;
-        else
+        else                                // 如果距离大于等于阈值，则取消匹配关系
         {
             mvuRight[vDistIdx[i].second]=-1;
             mvDepth[vDistIdx[i].second]=-1;
